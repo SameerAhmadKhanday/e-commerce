8f848bb8992c6917116505f5578608231cf4edff
Sameer <SameerAhmadKhanday>
2025-07-16 16:14:37 +0530

    developed admin side to add items

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,14 @@
+import adminlogo from './adminlogo.png'
+import plus from './plus.png'
+import tick from './tick.png'
+import upload from './upload.png'
+import listitems from './listitems.png'
+
+
+export const assets = {
+    adminlogo,
+    plus,
+    tick,
+    upload,
+    listitems
+}
